b1b1f4e17dd3ea71e38e024867afff6ebd8999a1
DatNX <117743824+Rayloveyou@users.noreply.github.com>
2024-06-01 14:27:03 +0700

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `email_id` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
